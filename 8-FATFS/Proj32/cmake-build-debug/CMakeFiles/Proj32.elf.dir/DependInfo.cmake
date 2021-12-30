820184b5374a090a95424ec4b3a20e75ebadfc25
hugh <hugh@Mac-mini.local>
2021-12-30 12:54:38 +0800

    V1.6

---
+++ 8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/DependInfo.cmake
@@ -0,0 +1,95 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Core/Startup/startup_stm32f103vbtx.s" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Core/Startup/startup_stm32f103vbtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../FATFS/Target"
+  "../FATFS/App"
+  "../Middlewares/Third_Party/FatFs/src"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Core/Src/bsp_spi_flash.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Core/Src/bsp_spi_flash.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Core/Src/main.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Core/Src/main.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Core/Src/stm32f1xx_hal_msp.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Core/Src/stm32f1xx_it.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Core/Src/syscalls.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Core/Src/syscalls.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Core/Src/sysmem.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Core/Src/sysmem.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Core/Src/system_stm32f1xx.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_spi.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_spi.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/FATFS/App/fatfs.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/FATFS/App/fatfs.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/FATFS/Target/user_diskio.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/FATFS/Target/user_diskio.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Middlewares/Third_Party/FatFs/src/diskio.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Middlewares/Third_Party/FatFs/src/diskio.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Middlewares/Third_Party/FatFs/src/ff.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Middlewares/Third_Party/FatFs/src/ff.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c.obj"
+  "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/Middlewares/Third_Party/FatFs/src/option/syscall.c" "/Users/hugh/Documents/Projects/NebulaPi32/8-FATFS/Proj32/cmake-build-debug/CMakeFiles/Proj32.elf.dir/Middlewares/Third_Party/FatFs/src/option/syscall.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../FATFS/Target"
+  "../FATFS/App"
+  "../Middlewares/Third_Party/FatFs/src"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
